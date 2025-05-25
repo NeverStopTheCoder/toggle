0420297c38abd23e6225a76f42fedfbf0f62e1a2
NeverStopTheCoder <jenny_fullerton@yahoo.com>
2025-05-25 09:28:43 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-let Toggle = toggle.createSlider3(2, 8)
-let Toggle2 = toggle.createSlider3(2, 9)
-toggle.setSliderPosition3(Toggle2, 80, 13)
-let Toggle3 = toggle.createSlider3(2, 10)
-toggle.setSliderPosition3(Toggle3, 80, 100)
-toggle.cycleToNextSlider()
+
